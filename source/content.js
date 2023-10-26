bef72d9971b021a3c4283abc82d5cfd70068d679
Kevin Whitley <krwhitley@gmail.com>
2023-10-26 13:42:05 -0500

    template update with dev mode

--- source/content.js
+++ source/content.js
@@ -2,6 +2,30 @@ import optionsStorage from './options-storage.js';
 
 console.log('💈 Content script loaded for', chrome.runtime.getManifest().name);
 
+
+// Function to get the selected text
+function getSelectedText() {
+  const selection = window.getSelection();
+  if (selection && selection.toString().trim() !== '') {
+    return selection.toString();
+  }
+  return null;
+}
+
+// Function to display selected text in an alert when the keyboard shortcut is pressed
+function handleKeyPress(event) {
+  if (
+    (event.ctrlKey) && // Check for Control/Command key
+    (event.altKey || event.metaKey) && // Check for Alt key
+    event.key === 'a' // Check for 'A' key
+  ) {
+    const selectedText = getSelectedText();
+    if (selectedText) {
+      alert(`Selected Text:\n${selectedText}`);
+    }
+  }
+}
+
 async function init() {
 	const options = await optionsStorage.getAll();
 	const color = `rgb(${options.colorRed}, ${options.colorGreen},${options.colorBlue})`;
@@ -12,6 +36,13 @@ async function init() {
 	notice.id = 'text-notice';
 	notice.style.border = '2px solid ' + color;
 	notice.style.color = color;
+
+  const prepend = document.createElement('div');
+	prepend.innerHTML = "injected";
+	document.body.prepend(prepend);
+
+  // Listen for keydown events
+  document.addEventListener('keydown', handleKeyPress);
 }
 
 init();
